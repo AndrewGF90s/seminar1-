c57fafe40ce8eca3cec46749f236eb777ddd896e
andy <andyggggg>
2024-02-12 15:51:57 +0300

    Task work

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите первое число");
+int FirstNumber =int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int SecondNumber =int.Parse(Console.ReadLine());
+int HightNumber = 0;
+int SmallNumber = 0;
+ 
+    if (FirstNumber > SecondNumber) {
+    HightNumber = FirstNumber;
+    SmallNumber = SecondNumber;
+     Console.WriteLine("Большее число " + HightNumber);
+    Console.WriteLine("Меньшее число " + SmallNumber);
+    }
+ 
+    else if (FirstNumber < SecondNumber) {
+           
+        HightNumber = SecondNumber;
+        SmallNumber = FirstNumber;
+        
+    Console.WriteLine("Большее число " + HightNumber);
+    Console.WriteLine("Меньшее число " + SmallNumber);
+    }
+    else {
+        Console.WriteLine("Числа ровны");
+        
+    }
