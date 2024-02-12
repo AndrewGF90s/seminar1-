7b4e1f4be49f16e2bab33b550d95fd812b999286
andy <andyggggg>
2024-02-12 16:24:35 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int ImputNumber1 = int.Parse(Console.ReadLine());
+int ImputNumber2 = int.Parse(Console.ReadLine());
+int ImputNumber3 = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (max < ImputNumber1) {
+     max = ImputNumber1;
+}
+if (max < ImputNumber2) {
+    max = ImputNumber2;
+}
+if (max < ImputNumber3) {
+    max = ImputNumber3;
+}
+Console.WriteLine(max);
