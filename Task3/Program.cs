c7a661718e06852b5bd0219bfe96a467c3ba33da
andy <andyggggg>
2024-02-12 16:42:29 +0300

    add work in Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿ Console.WriteLine("ВВедите число ");
+int number = int.Parse(Console.ReadLine());
+int check = number % 2;
+ 
+if ( check == 1 ) {
+    Console.WriteLine("Число " + number + " Нечетное" );
+}
+else {
+    Console.WriteLine("Число " + number + " Четное");
+}
